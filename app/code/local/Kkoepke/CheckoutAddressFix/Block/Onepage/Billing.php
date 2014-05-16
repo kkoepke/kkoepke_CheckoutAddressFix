8fe2acbf4f1d06632b2b1c3e10aa48c9893d3289
kkoepke <kkoepke@scm.lupcom.de>
2014-05-16 14:17:06 +0200

    Initial commit

---
+++ app/code/local/Kkoepke/CheckoutAddressFix/Block/Onepage/Billing.php
@@ -0,0 +1,26 @@
+<?php
+class Kkoepke_CheckoutAddressFix_Block_Onepage_Billing extends Mage_Checkout_Block_Onepage_Billing {
+
+	protected $_address;
+	protected $_taxvat;
+
+	public function getAddress() {
+		if(is_null($this->_address)) {
+			if($this->isCustomerLoggedIn()) {
+				$this->_address = $this->getQuote()->getBillingAddress();
+				if(!$this->_address->getFirstname()) {
+					$this->_address->setFirstname($this->getQuote()->getCustomer()->getFirstname());
+				}
+				if(!$this->_address->getLastname()) {
+					$this->_address->setLastname($this->getQuote()->getCustomer()->getLastname());
+				}
+			} else {
+				$this->_address = $this->getQuote()->getBillingAddress();
+			}
+		}
+
+		return $this->_address;
+	}
+
+}
+?>
---
+++ app/code/local/Kkoepke/CheckoutAddressFix/Block/Onepage/Shipping.php
@@ -0,0 +1,14 @@
+<?php
+class Kkoepke_CheckoutAddressFix_Block_Onepage_Shipping extends Mage_Checkout_Block_Onepage_Shipping {
+
+    public function getAddress()
+    {
+        if (is_null($this->_address)) {
+            $this->_address = $this->getQuote()->getShippingAddress();
+        }
+
+        return $this->_address;
+    }
+
+}
+?>
